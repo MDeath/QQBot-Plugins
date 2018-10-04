90f262be95b7b58d029d8bdd5a3aaea09b04acba
M_Die <42289879+MDeath@users.noreply.github.com>
2018-10-04 12:27:12 +0800

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -14,7 +14,7 @@ def onUnplug(bot):
         bot.SendTo(g, '已卸载 repeat')
 
 def onQQMessage(bot, contact, member, content):
-    if not bot.isMe(contact, member) or member != None:
+    if not bot.isMe(contact, member) and member != None:
         if random.randint(0,500) < 1:
             bot.SendTo(contact, content)
         elif random.randint(0,1) < 1:
@@ -24,13 +24,13 @@ def onQQMessage(bot, contact, member, content):
             user3, user4 = info['user'][3], info['user'][4]
             text, text1, text2 = info['text'][0] , info['text'][1], info['text'][2]
             text3, text4 = info['text'][3], info['text'][4]
-            if randint == 4 and user4 != member.uin and content == text4:
+            if randint < 2 and user4 != member.uin and content == text4:
                 bot.SendTo(contact, content)
-            elif randint == 3 and user3 != user4 and text3 == text4 != text:
+            elif randint == 3 and user3 != user4 != member.uin and text4 == text3 != text2:
                 bot.SendTo(contact, text4)
-            elif randint == 2 and user2 != user3 and text2 == text3 != text:
+            elif randint == 2 and user2 != user3 != member.uin and text3 == text2 != text1:
                 bot.SendTo(contact, text3)
-            elif randint == 1 and user1 != user2 and text1 == text2 != text:
-                bot.SendTo(content, text2)
-            elif randint == 0 and user != user1 and text == text1:
-                bot.SendTo(content, text1)
+            elif randint == 1 and user1 != user2 != member.uin and text2 == text1 != text:
+                bot.SendTo(contact, text2)
+            elif randint == 0 and user != user1 != member.uin and text == text1:
+                bot.SendTo(contact, text1)
